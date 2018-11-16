14575fbba8c5ee2e6a80012653f39b337e40dff2
Joshua <ehlodex@users.noreply.github.com>
2018-11-16 13:46:48 -0500

    Update anah.py
    Added language_pack[].conjunction

--- anah.py
+++ anah.py
@@ -4,6 +4,7 @@ anah_version = "0.320.1331"
 class language_data:
     name = ""
     greeting = ""
+    conjunction = ""
     meetings = {}
 
 language_pack = {
@@ -14,6 +15,7 @@ language_pack = {
 ## English
 language_pack["en"].name = "English"
 language_pack["en"].greeting = "Hello"
+language_pack["en"].conjunction = "and"
 language_pack["en"].meetings = {
         "Sundays at": ["10:00 am","11:15 am","7:00 pm"],
         "Wednesdays at": ["7:00 pm"]
@@ -22,6 +24,7 @@ language_pack["en"].meetings = {
 ## Español
 language_pack["es"].name = "Español"
 language_pack["es"].greeting = "Hola"
+language_pack["es"].conjunction = "y"
 language_pack["es"].meetings = {
         "Domingos a las": ["10:00 am","11:15 am","7:00 pm"],
         "Miercoles a las": ["7:00 pm"]
@@ -33,12 +36,12 @@ def get_meetings(lang):
     for day in meetings:
         out_msg = out_msg + day
         if len(meetings[day]) == 1:
-            out_msg = out_msg + " " + meetings[day][0]
+            out_msg = "{} {}".format(out_msg, meetings[day][0])
         elif len(meetings[day]) == 2:
-            out_msg = out_msg + " " + meetings[day][0] + " and " + meetings[day][1]
+            out_msg = "{} {} {} {}".format(out_msg, meetings[day][0], language_pack[lang].conjunction, meetings[day][1])
         else:
             out_msg = out_msg + " " + ', '.join(map(str, meetings[day][0:-1]))
-            out_msg = out_msg + ", and " + meetings[day][-1]
+            out_msg = "{}, {} {}".format(out_msg, language_pack[lang].conjunction, meetings[day][-1]
         out_msg = out_msg + ". "
     return out_msg
 
