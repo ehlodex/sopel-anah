6b871cd135ed27b606e7569b94a8e3ba35473df0
Joshua <ehlodex@users.noreply.github.com>
2018-11-15 19:50:23 -0500

    Update anah.py
    Partial update for static meeting times

--- anah.py
+++ anah.py
@@ -17,6 +17,24 @@ services = {
     "en": {"Sundays": ["10:00 am","11:15 am","7:00 pm"], "Wednesdays": ["7:00 pm"]},
     "es": {"Domingos": ["10:00 am","11:15 am","7:00 pm"], "Miercoles": ["7:00 pm"]}
 }
+services_msg = {
+    "en": "",
+    "es": ""
+}
+
+def get_services(myLanguage):
+    days = services[myLanguage]
+    out_msg = services_msg[myLanguage]
+    for day in days:
+        if len(days[day]) == 1:
+            day_msg = day + " at " + days[day][0]
+        elif len(days[day]) == 2:
+            day_msg = day + " at " + days[day][0] + " and " + days[day][1]
+        else:
+            day_msg = day + " at " + ', '.join(days[day][0:-1]) + ", and " + days[day][-1]
+        day_msg = day_msg + ". "
+        out_msg = out_msg +  day_msg
+    return out_msg
 
 @sopel.module.commands('anah')
 def modver(bot, trigger):
@@ -24,32 +42,8 @@ def modver(bot, trigger):
 
 @sopel.module.rule('.*(time.*(service|meeting))|((service|meeting).*time).*')
 def hours_en(bot, trigger):
-    # you can add a message prefix here
-    services_msg = ""
-    days = services["en"]
-    for day in days:
-        if len(days[day]) == 1:
-            day_msg = day + " at " + days[day][0]
-        elif len(days[day]) == 2:
-            day_msg = day + " at " + days[day][0] + " and " + days[day][1]
-        else:
-            day_msg = day + " at " + ', '.join(days[day][0:-1]) + ", and " + days[day][-1]
-        day_msg = day_msg + ". "
-        services_msg = services_msg +  day_msg
-    bot.say(services_msg)
+    bot.say(get_services("en"))
 
 @sopel.module.rule('.*(hora.*(servicio|reunion))|((servicio|reunion).*hora).*')
 def hours_en(bot, trigger):
-    # you can add a message prefix here
-    services_msg = ""
-    days = services["es"]
-    for day in days:
-        if len(days[day]) == 1:
-            day_msg = day + " at " + days[day][0]
-        elif len(days[day]) == 2:
-            day_msg = day + " at " + days[day][0] + " and " + days[day][1]
-        else:
-            day_msg = day + " at " + ', '.join(days[day][0:-1]) + ", and " + days[day][-1]
-        day_msg = day_msg + ". "
-        services_msg = services_msg +  day_msg
-    bot.say(services_msg)
+    bot.say(get_services("es"))
